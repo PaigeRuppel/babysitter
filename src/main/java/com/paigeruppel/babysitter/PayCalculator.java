59a2d2a053f56d1ae2fda97724672c3f16546e4b
paigeruppel <paige.ruppel@gmail.com>
2017-08-23 15:58:48 -0400

    wrote and passed first three tests for validating start time

---
+++ src/main/java/com/paigeruppel/babysitter/PayCalculator.java
@@ -0,0 +1,9 @@
+package com.paigeruppel.babysitter;
+
+public class PayCalculator {
+
+	public boolean isValidStart(int start) {
+		return start >= 17;
+	}
+
+}
---
+++ src/test/java/com/paigeruppel/babysitter/PayCalculatorTest.java
@@ -0,0 +1,28 @@
+package com.paigeruppel.babysitter;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PayCalculatorTest {
+
+	
+	@Test
+	public void shouldReturn5PmAsValidStartTime() {
+		PayCalculator underTest = new PayCalculator();
+		assertThat(underTest.isValidStart(17), is(true));
+	}
+	
+	@Test
+	public void shouldReturn4PmAsNotValidStartTime() {
+		PayCalculator underTest = new PayCalculator();
+		assertThat(underTest.isValidStart(16), is(false));
+	}
+	
+	@Test
+	public void shouldReturn6PmAsValidStartTime() {
+		PayCalculator underTest = new PayCalculator();
+		assertThat(underTest.isValidStart(18), is(true));
+	}
+}
